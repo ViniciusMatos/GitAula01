9b6377d8ff171de9fa6b884e834b790a25af9790
ViniciusMatos <vmatos12@gmail.com>
2021-08-24 15:39:15 -0300

    Testando nova branch

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,9 +1,12 @@
+import javax.swing.JOptionPane;
 
 public class Exemplo {
 	public static void main(String args[]) {
 		
 		System.out.println("Hello World");
-		System.out.println("Aluna concluida");
+		int idade = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite sua idade"));
+		
+		System.out.println("Idade do usuário" +idade);
 	}
 
 }
